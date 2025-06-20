3760acd24ae2e08c96f1750d714938a05134dbfa
Ashley <motsieashley31@gmail.com>
2025-06-20 20:54:44 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import secrets
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation # Automatically include all character types
+
+    if not characters:
+        raise ValueError("No characters available for password generation.")
+
+    password = ''.join(secrets.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    print("Random Password Generator")
+
+    try:
+        length = int(input("Enter desired password length(e.g. 12): "))
+        if length <= 0:
+            raise ValueError("Length must be a positive number!")
+
+        password = generate_password(length)
+        print(f"\nYour generated password is:\n{password}")
+
+    except ValueError as e:
+        print(f"Error: {e}")
+
+    print("\nThank you for using the Password Generator!")
+
+if __name__ == "__main__":
+    main()
